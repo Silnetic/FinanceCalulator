52d6d64deb7897e902c3d592f467f7381ec3d013
Silnetic <62838763+Silnetic@users.noreply.github.com>
2020-04-24 20:14:36 +0100

    Updated Login.py
    Completed the interface for the login screen I will now need to add functionlity for the buttons.

--- login.py
+++ login.py
@@ -12,8 +12,16 @@ class App:
     def __init__(self, master):
         # Configeration
         self.master = master
-        self.style = ttk.Style()
-        self.style.configure("BW.TLabel", foreground="black")
+
+        # Label Style
+        self.label_style = ttk.Style()
+        self.label_style.configure("BW.TLabel", foreground="black",
+                                   font=("Open Sans", 20))
+
+        self.btn_style = ttk.Style()
+        self.btn_style.configure("BW.TButton", font=("Open Sans", 20),
+                                 width=20)
+
         self.master.title("Login")
 
         # Text Variables
@@ -21,8 +29,7 @@ class App:
         self.password = tkinter.StringVar()
 
         # Username
-        self.user_label = ttk.Label(text="Username", style="BW.TLabel",
-                                    font=("Open Sans", 20))
+        self.user_label = ttk.Label(text="Username", style="BW.TLabel")
         self.user_label.grid(row=0, column=0, padx=8, pady=20)
 
         self.user_entry = ttk.Entry(textvariable=self.username,
@@ -30,19 +37,20 @@ class App:
         self.user_entry.grid(row=0, column=1, padx=8, pady=20)
 
         # Password
-        self.password_label = ttk.Label(text="Password", style="BW.TLabel",
-                                        font=("Open Sans", 20))
+        self.password_label = ttk.Label(text="Password", style="BW.TLabel")
         self.password_label.grid(row=1, column=0, padx=8, pady=20)
 
         self.user_entry = ttk.Entry(textvariable=self.password,
                                     font=("Open Sans", 20), show="*")
         self.user_entry.grid(row=1, column=1, padx=8, pady=20)
-    
 
+        # Login Button
+        self.login_btn = ttk.Button(text="Login", style="BW.TButton")
+        self.login_btn.grid(row=2, column=0, padx=8, pady=20)
 
-
-
-
+        # Register Button
+        self.register_btn = ttk.Button(text="Register", style="BW.TButton")
+        self.register_btn.grid(row=2, column=1, padx=8, pady=20)
 
 
 if __name__ == "__main__":
